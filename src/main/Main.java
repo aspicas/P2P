850a3f104afb70ac1bb38f3c2f3d368827d0003f
David Garcia <david.a.g.r93@gmail.com>
2016-05-19 10:49:10 -0400

    Definicion del main del programa

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created by david on 5/19/16.
+ * https://www.programarya.com/Cursos/Java-Avanzado/Sockets
+ */
+
+public class Main {
+    public static void main (String[] args)
+    {
+        System.out.print("Hola mundo");
+    }
+}
--- src/main/Usuario.java
+++ src/main/Usuario.java
@@ -1,5 +1,9 @@
+package main;
+
 /**
  * Created by david on 5/18/16.
  */
+
 public class Usuario {
+
 }
