7088dea5664ddae0dd226624cfceccd555485f19
David Garcia <david.a.g.r93@gmail.com>
2016-05-19 20:22:35 -0400

    Prueba del main con la conexion

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,13 @@ package main;
  * Created by david on 5/19/16.
  * https://www.programarya.com/Cursos/Java-Avanzado/Sockets
  */
+import java.io.IOException;
+import java.net.*;
 
 public class Main {
-    public static void main (String[] args)
+    public static void main (String[] args) throws IOException
     {
-        System.out.print("Hola mundo");
+        InetAddress address = InetAddress.getLocalHost();
+        System.out.print(address);
     }
 }
--- src/main/Usuario.java
+++ src/main/Usuario.java
@@ -1,9 +1,20 @@
 package main;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  * Created by david on 5/18/16.
  */
 
 public class Usuario {
 
+    public int puerto = 50000;
+    public Socket cs; //Cliente socket
+    public ServerSocket ss; //Server socket
+
+    public Usuario() throws IOException {
+        ss = new ServerSocket(puerto); //Crea el socket para el puerto 50000
+        cs = new Socket(); //Socket para el cliente
+    }
 }
