6706beef4604eb23abfbd8d634ac3b8bec443ee3
David Garcia <david.a.g.r93@gmail.com>
2016-05-19 23:30:19 -0400

    Hash en la IP

--- src/main/Usuario.java
+++ src/main/Usuario.java
@@ -13,21 +13,24 @@ public class Usuario {
     public int puerto = 50000;
     public Socket cs; //Cliente socket
     public ServerSocket ss; //Server socket
-    public String address;
+    public int address;
 
     public Usuario() throws IOException {
         ss = new ServerSocket(puerto); //Crea el socket para el puerto 50000
         cs = new Socket(); //Socket para el cliente
-        address = "";
+        address = 0;
     }
 
     public void findIP() throws IOException {
         Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces(); //Encuentra interfaz
-        NetworkInterface e = n.nextElement(); // la separa de la numeracion y la coloca aparte.
+
+        /**/NetworkInterface e = n.nextElement(); // la separa de la numeracion y la coloca aparte.
         Enumeration<InetAddress> a = e.getInetAddresses(); //Encuentra la MAC
         InetAddress addr = a.nextElement(); // la separa de la numeracion y la coloca aparte.
         addr = a.nextElement(); //Seguido de la MAC esta la IP
-        address = addr.getHostAddress().toString();
+        //address = addr.hashCode();
+        address = addr.getHostAddress().hashCode();//*/
+
         /* http://stackoverflow.com/questions/19476872/java-get-local-ip
         for (; n.hasMoreElements();)
         {
@@ -36,13 +39,12 @@ public class Usuario {
             for (; a.hasMoreElements();)
             {
                 InetAddress addr = a.nextElement();
-                System.out.println("  " + a.nextElement());
-                //System.out.println("  " + addr.getHostAddress());
+                System.out.println("  " + addr.getHostAddress());
             }
         }//*/
     }
 
-    public String getAddress() {
+    public int getAddress() {
         return address;
     }
 }
