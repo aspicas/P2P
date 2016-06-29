1b76bb27681255a3be0a64522928ffce2b335860
Gaby Rodriguez <gbsojo@gmail.com>
2016-06-29 10:02:00 -0430

    Receiver

--- src/main/Receiver.java
+++ src/main/Receiver.java
@@ -12,12 +12,15 @@ import java.nio.file.Files;
 /**
  * Created by gbsojo on 6/11/16.
  * http://www.java2s.com/Code/Java/Network-Protocol/TransferafileviaSocket.htm
+ * http://www.rgagnon.com/javadetails/java-0542.html
  */
 public class Receiver extends Thread{
     public Socket socket = null;
     public int idSession;
+    public String file;
 
-    public Receiver() {
+    public Receiver(String file) {
+        this.file = file;
         try {
             this.socket = new Socket("127.0.0.1", 3000);
         } catch (IOException ex) {
@@ -26,21 +29,35 @@ public class Receiver extends Thread{
     }
 
 
-    public void receiveResource throws IOException {
-        byte[] mybytearray = new byte[1024];
-        InputStream is = sock.getInputStream();
-        FileOutputStream fos = new FileOutputStream("s.pdf");
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        int bytesRead = is.read(mybytearray, 0, mybytearray.length);
-        bos.write(mybytearray, 0, bytesRead);
-        bos.close();
-        sock.close();
+    public void receiveFile () throws IOException {
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        try {
+            byte[] mybytearray = new byte[1024];
+            InputStream is = socket.getInputStream();
+            fos = new FileOutputStream(this.file);
+            bos = new BufferedOutputStream(fos);
+            int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+            int current = bytesRead;
+            do {
+                bytesRead =
+                        is.read(mybytearray, current, (mybytearray.length - current));
+                if (bytesRead >= 0) current += bytesRead;
+            } while (bytesRead > -1);
+
+            bos.write(mybytearray, 0, bytesRead);
+            bos.flush();
+        }
+        finally {
+            if (fos != null) fos.close();
+            if (bos != null) bos.close();
+            if (socket != null) socket.close();
+        }
     }
 
 
     @Override
-    public void run(){
-        Listening();
+    public void run() {
     }
 
 }
