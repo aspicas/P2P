243058a30eb7dc4d0d3b0ae904966fa8c2fe4061
Gaby Rodriguez <gbsojo@gmail.com>
2016-06-29 09:08:26 -0430

    Sender recibe file en constructor

--- src/main/Sender.java
+++ src/main/Sender.java
@@ -23,13 +23,15 @@ public class Sender extends Thread{
     public int idSession;
     public DataOutputStream output = null;
     public DataInputStream input = null;
+    public String file;
 
-    public Sender() {
+    public Sender(String file) {
         try {
             this.server = new ServerSocket(port);
             this.idSession = 0;
             this.input = new DataInputStream(client.getInputStream());
             this.output = new DataOutputStream(client.getOutputStream());
+            this.file = file;
         }
         catch (IOException ex) {
             Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE,null,ex);
@@ -49,7 +51,7 @@ public class Sender extends Thread{
         }
     }
 
-    public void sendFile(String file) throws IOException {
+    public void sendFile() throws IOException {
         FileInputStream fis;
         BufferedInputStream bis = null;
         OutputStream os = new OutputStream() {
@@ -60,7 +62,7 @@ public class Sender extends Thread{
         };
 
         try {
-            File myFile = new File(file);
+            File myFile = new File(this.file);
             byte[] mybytearray = new byte[(int) myFile.length()];
             fis = new FileInputStream(myFile);
             bis = new BufferedInputStream(fis);
@@ -75,23 +77,12 @@ public class Sender extends Thread{
         }
     }
 
-    public int sendFileSize (String file) {
-        File myFile = new File (file);
+    public int sendFileSize () {
+        File myFile = new File (this.file);
         byte [] mybytearray  = new byte [(int)myFile.length()];
         return mybytearray.length;
     }
 
-    public String[] defineAction(String str){
-        StringTokenizer st = new StringTokenizer(str, " ");
-        // itera mediante el “objeto st” para obtener más tokens de él
-        String[] token = new String[st.countTokens()];
-        int i = 0;
-        while (st.hasMoreElements()) {
-            token[i] = st.nextElement().toString();
-            i++;
-        }
-        return token;
-    }
 
     @Override
     public void run(){
